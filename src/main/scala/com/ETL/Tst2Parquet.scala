4839688979ef2d97fc882926bca150723284bcf3
SmorSmor <1223882390@qq.com>
2019-08-20 17:19:51 +0800

    day02代码

--- src/main/scala/com/ETL/Tst2Parquet.scala
+++ src/main/scala/com/ETL/Tst2Parquet.scala
@@ -7,23 +7,29 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object Tst2Parquet {
   def main(args: Array[String]): Unit = {
+
     // 判断路径是否正确
     if(args.length != 2){
       println("目录参数不正确，退出程序")
       sys.exit()
     }
+
     // 创建一个集合保存输入和输出目录
     val Array(inputPath,outputPath) = args
     val conf = new SparkConf().setAppName(this.getClass.getName).setMaster("local[*]")
-      // 设置序列化方式 采用Kyro序列化方式，比默认序列化方式性能高
       .set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
+    // 设置序列化方式 采用Kyro序列化方式，比默认序列化方式性能高
+
     // 创建执行入口
     val sc = new SparkContext(conf)
     val sQLContext = new SQLContext(sc)
+
     // 设置压缩方式 使用Snappy方式进行压缩
     sQLContext.setConf("spark.sql.parquet.compression.codec","snappy")
+
     // 进行数据的读取，处理分析数据
     val lines = sc.textFile(inputPath)
+
     // 按要求切割，并且保证数据的长度大于等于85个字段，
     // 如果切割的时候遇到相同切割条件重复的情况下，需要切割的话，那么后面需要加上对应匹配参数
     // 这样切割才会准确 比如 ,,,,,,, 会当成一个字符切割 需要加上对应的匹配参数
